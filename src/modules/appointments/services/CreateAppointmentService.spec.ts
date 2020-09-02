354a06dcd9520ab1e017ebddd0994061c10c4851
Lauro Costa <joaolauro94@gmail.com>
2020-09-01 22:47:53 -0300

    add user_id in appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,6 +16,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '123321',
       provider_id: '123123',
     });
 
@@ -28,6 +29,7 @@ describe('CreateAppointment', () => {
 
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '123321',
       provider_id: '123123',
     });
 
@@ -35,6 +37,7 @@ describe('CreateAppointment', () => {
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '123123',
+        user_id: '123321',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
